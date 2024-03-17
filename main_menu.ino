90a83772003f31fbcd95e4c4d562732d22bb1e5e
Leonardo Sousa <leonardoiself2@gmail.com>
2024-03-16 23:02:18 -0300

    Menu funcional com icones e titulos com base nos arrays

--- main_menu.ino
+++ main_menu.ino
@@ -1,32 +1,53 @@
 void screenMainMenu(){
   display.clearDisplay();
-  
-  String title = "Dispositivo de Saida";
+  encoderIsValue = false;
+  cursor = 0;
 
-  //Backgrounds Icons
-  display.drawBitmap(46, 0, bgBigIcon_36_36, 36, 36, WHITE);
-  display.drawBitmap(9, 4, bgSmallIcon_28_28, 28, 28, WHITE);
-  display.drawBitmap(91, 4, bgSmallIcon_28_28, 28, 28, WHITE);
-
-  //Draw Icons
-  display.drawBitmap(13, 10, icon_volume_20_16, 20, 16, BLACK);
-  display.drawBitmap(54, 10, icon_speaker_device_19_16, 19, 16, BLACK);
-  display.drawBitmap(97, 10, icon_plane_16_16, 16, 16, BLACK);
+  const unsigned char* icons[] = {icon_volume_20_20, icon_speaker_device_20_20, icon_plane_20_20, icon_settings_20_20};
+  String titles[] = {"Volume Mixer", "Dispositivo de Saida", "Flight Simulator", "Configuracoes"};
 
 
-  //Print title
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  centerText(title, 57);
+
+  int sizeTitle = (sizeof(titles) / sizeof(titles[0])) - 1;
+  while(true){
+    display.clearDisplay();
+
+    //Backgrounds Icons
+    display.fillRoundRect(45, 0, 38, 38, 2, WHITE);
+    display.drawRoundRect(0, 4, 30, 30, 1, WHITE);
+    display.drawRoundRect(98, 4, 30, 30, 1, WHITE);
+
+    cursor = cursor > sizeTitle ? cursor = 0 : cursor;
+    cursor = cursor < 0 ? cursor = sizeTitle : cursor;
+    Serial.println(cursor);
+
+    //Previous icon
+    int previousCursor = (cursor - 1) < 0 ? sizeTitle : cursor - 1;
+    display.drawBitmap(5, 9, icons[previousCursor], 20, 20, WHITE);
+
+    //Current icon
+    display.drawBitmap(54, 9, icons[cursor], 20, 20, BLACK);
+
+    //Next icon
+    int nextCursor = (cursor + 1) > sizeTitle ? 0 : cursor + 1;
+    display.drawBitmap(103, 9, icons[nextCursor], 20, 20, WHITE);
+
+
+    //Print title
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    centerText(titles[cursor], 55);
+
+    display.display();
+  }
 
 
   //Selector
-  display.fillCircle(69, 45, 1, WHITE);
-  display.fillCircle(63, 45, 1, WHITE);
-  display.fillCircle(57, 45, 1, WHITE);
-  display.drawRect(62, 44, 3, 3, WHITE);
+  // display.fillCircle(69, 45, 1, WHITE);
+  // display.fillCircle(63, 45, 1, WHITE);
+  // display.fillCircle(57, 45, 1, WHITE);
+  // display.drawRect(62, 44, 3, 3, WHITE);
 
-  display.display();
 }
 
 
